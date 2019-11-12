da7015aa1c20a524fb8f5b0b8800cd806f1f8b83
Jonathan Archer <jonathan.d.archer@gmail.com>
2019-11-12 12:51:00 -0500

    added day 12 challenge

---
+++ lhl21day/12-dirtyOrClean.js
@@ -0,0 +1,43 @@
+/*
+Instructions
+For this challenge we will implement a function called checkAir(), which will check a collection of air samples. The function will take in two arguments. The first argument is an array of strings, where each string represents a small air sample that is either clean or dirty. The second argument is a number representing the highest acceptable amount of dirty samples. For example, a threshold of 0.4 means that there must be less than 40% of total samples classified as dirty for our air to be considered clean. Our function must return Polluted if there are too many dirty air samples, or Clean if the proportion of dirty samples is below the threshold.
+
+*/
+
+const checkAir = function(samples, threshold) {
+  let dirtyCount = 0;
+  for (let sample of samples) {
+    if (sample === "dirty") {
+      dirtyCount += 1;
+    }
+  }
+
+  let ratioDirty = dirtyCount / samples.length;
+
+  if (ratioDirty >= threshold) {
+    return "Polluted";
+  } else {
+    return "Clean";
+  }
+};
+
+// example
+
+const samples = [
+  "clean",
+  "clean",
+  "dirty",
+  "clean",
+  "dirty",
+  "clean",
+  "clean",
+  "dirty",
+  "clean",
+  "dirty"
+];
+
+const threshold = 0.3;
+
+// Expexted Output: Polluted
+
+checkAir(samples, threshold);
